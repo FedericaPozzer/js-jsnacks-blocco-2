2745392cb1c6dae7883ae4f14873ff223dfe5494
Federica Pozzer <federica.pozzer@gmail.com>
2023-01-27 15:10:24 +0100

    Jsnack1 done (in console)

--- JSnack1/js/script.js
+++ JSnack1/js/script.js
@@ -2,3 +2,12 @@
 
 // JSnack1
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+const user_number = parseInt(prompt("inserisci un numero!"));
+// console.log(user_number);
+
+if (user_number % 2) {
+    console.log(user_number + 1);
+} else {
+    console.log(user_number);
+}
