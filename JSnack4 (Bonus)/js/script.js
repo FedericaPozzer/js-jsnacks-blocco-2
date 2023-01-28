4e9032e974f392c764361acd723bb04a2217324e
Federica Pozzer <federica.pozzer@gmail.com>
2023-01-28 16:04:44 +0100

    JSnack4 done

--- JSnack4 (Bonus)/js/script.js
+++ JSnack4 (Bonus)/js/script.js
@@ -1,4 +1,14 @@
 // console.log("Hello Kikka");
 
 // JSnack4(Bonus)
-// Crea due array che hanno un numero di elementi diversi.Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+// Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+
+const lista_breve = [1, 2, 3, 4, 5];
+const lista_lunga = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (i = 0; lista_breve.length < lista_lunga.length; i++) {
+    lista_breve.push(lista_breve[i]);
+}
+
+console.log("lista breve: " + lista_breve);
+console.log("lista lunga: " + lista_lunga);
